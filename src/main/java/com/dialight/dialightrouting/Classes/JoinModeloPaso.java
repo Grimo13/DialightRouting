b51b4b90d30cc2ae3985918789c9169e7af7950a
Grimo13 <grimox13@gmail.com>
2013-11-14 17:29:38 -0800

    Creacion de clases

---
+++ src/main/java/com/dialight/dialightrouting/Classes/JoinModeloPaso.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.dialight.dialightrouting.Classes;
+
+/**
+ *
+ * @author Jorge
+ */
+public class JoinModeloPaso {
+    private int id;
+    private Modelo modelo;
+    private Paso paso;
+    private int numerodepasos;
+    
+}
