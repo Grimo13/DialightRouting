be41ef4c1db953ca812f704be00439f0501ba413
Grimo13 <grimox13@gmail.com>
2013-11-14 16:57:57 -0800

    Dummy

--- src/main/java/com/dialight/dialightrouting/FXMLController.java
+++ src/main/java/com/dialight/dialightrouting/FXMLController.java
@@ -16,6 +16,7 @@ public class FXMLController implements Initializable {
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Bienvenido a Dialight");
+        label.setText("OERIJGOEIJGOEIRJG");
     }
     
     @Override
