9337e8a3624eb487c2d7433d157f25fb5b8374c0
Ricardo Velazquez <chamuco_1111@hotmail.com>
2013-12-03 18:50:50 -0800

    Archivos xml

--- src/main/java/com/dialight/dialightrouting/Classes/Paso.java
+++ src/main/java/com/dialight/dialightrouting/Classes/Paso.java
@@ -4,5 +4,32 @@ public class Paso {
     int id;
     double tiempo;
     String nombre;
+
+    public Paso() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(double tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
     
 }
